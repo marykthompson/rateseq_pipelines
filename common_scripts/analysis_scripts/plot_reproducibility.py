fixed read count scaling. Used hierarchical indexing for df

common_scripts/analysis_scripts/plot_reproducibility.py
@@ -10,173 +10,114 @@ from import_file import *
 #2) plots containing each set of filtered genes
 #3) plots and summary file with spike-in mapping info --> now move the barplot code into module file
 #version 7: plot tpm reproducibility, not just rate reproducibility
+#version 8: fix it to use the rep hierarchical indexing sheet
 
-def plot_spikeins(big_df, genes_dict = None, outdir = None):
-    '''Count number of reads mapping to spike-in sets.
+def plot_spikeins(df, genes_dict = None, outdir = None):
+    '''
+    Count number of reads mapping to spike-in sets.
     Write summary to file and also plot bar graphs
     genes_dict = {'gene_set1':{}, 'gene_set2':{}}
-    Note: in future, maybe move this code to module, and import to do the same thing for rRNA mapping'''
+    Return the df with the spike-ins removed so that they won't be included in the gene reproducibility plots.
+    '''
 
-    big_df['txt'] = big_df.index
+    #get all spikein gene names
     gene_sets = list(genes_dict)
     all_genes = set().union(*list(genes_dict.values()))
 
     this_outdir = os.path.join(outdir, 'spike_ins')
-    if not os.path.exists(this_outdir):
-        os.makedirs(this_outdir)
+    os.makedirs(this_outdir, exist_ok = True)
 
-    #1) get component column names for each replicate, which are split into exon and intron
-    colnames = [i for i in big_df.columns.values if 'counts' in i]
-    d = defaultdict(list)
-    for i in colnames:
-        coldata = i.split('_')
-        exp, rep = coldata[0], coldata[-1]
-        newname = '%s_%s' % (exp, rep)
-        d[newname].append(i)
-    for k in d:
-        big_df[k] = big_df[d[k]].sum(axis = 1)
+    #get rep hierarchical index
+    reps = [i for i  in df.columns.levels[0] if i.startswith('rep')]
+    to_plot = [i for i in df.columns.levels[1] if 'counts' in i]
 
-    #2) Make dfs for each gene set and get percentages of mapping counts
-    #make a dataframe for each set of genes
-    df_dict = {}
-    #for gene set (i.e. erccs)
+    #make plot and summary table for each set of spikeins (ERCC, SIRV)
     for s in genes_dict:
-        df_dict[s] = big_df.loc[big_df.index.isin(genes_dict[s])].copy()
-        #make summary df of spike-ins
         count_dfs = []
         percent_dfs = []
-        #adjust the width of graph based on number of samples plotted:
-        #for experiment (e.g. total_1)
-        for exp in d:
-            #get the total of all counts for each experiment
-            total = big_df[exp].sum()
-            outname = os.path.join(this_outdir, '%s_%s_bar') % (s, exp)
-            #print('this df', df_dict[s])
-            if not df_dict[s].empty:
-                new_df = pipeline_aux.quick_barplot(data = df_dict[s], y_col = exp, x_col = 'txt', divide_by = total, percent = True, outname = outname, x_label = 'transcript', y_label = '% of mapping transcripts')
-            else:
-                new_df = df_dict[s]
-                new_df['percent'] = np.nan
-                new_df['frac'] = np.nan
-            count_dfs.append(new_df[[exp]])
-            new_df.rename(columns = {'percent':'percent_%s' % exp}, inplace = True)
-            percent_dfs.append(new_df[['percent_%s' % exp]])
+        for cat in to_plot:
+            for rep in reps:
+                outname = os.path.join(this_outdir, '_'.join([s, cat, rep, 'bar']))
+                total = df[(rep, cat)].sum()
+                data = df.loc[df.index.isin(genes_dict[s]), rep]
+                #add index to column for easy passing to seaborn
+                data['txt'] = data.index
+                expname = '%s_%s' % (cat, rep)
+                #if there are reads mapped to spikeins, then make plots and output files
+                if not data.empty:
+                    new_df = pipeline_aux.quick_barplot(data = data, y_col = cat, x_col = 'txt', divide_by = total, percent = True, outname = outname, x_label = 'transcript', y_label = '% of mapping transcripts')
+                else:
+                    new_df = data
+                    new_df['percent'] = np.nan
+                    new_df['frac'] = np.nan
+
+                new_df.rename(columns = {'percent': 'percent_%s' % expname, 'frac': 'frac_%s' % expname}, inplace = True)
+
+                count_dfs.append(new_df[['frac_%s' % expname]])
+                percent_dfs.append(new_df[['percent_%s' % expname]])
 
         #write summary output files
         summary_df = pd.concat(count_dfs + percent_dfs, axis = 1)
         summary_df.to_csv(os.path.join(this_outdir, '%s_spike_counts.csv' % s))
 
     #Remove ERCC and SIRV genes from dataset as we don't want to plot these for gene reproducibility
-    big_df.drop(labels = all_genes, inplace = True)
-    return big_df
+    df.drop(labels = all_genes, inplace = True)
 
 def plot_reproducibility(df, outdir = None, gene_list_dir = None, min_reads = None, min_read_type = None):
     '''Make scatterplots showing reproducibility between replicates and how many genes passed filter'''
 
-    #i) log transform synthesis and decay rates, make df = subset of columns with values
-    colnames = df.columns.values
-    synth_cols = [i for i in colnames if 'synthesis' in i]
-    deg_cols = [i for i in colnames if 'decay' in i]
-    proc_cols = [i for i in colnames if 'processing' in i]
-    foursu_exon_cols = [i for i in colnames if ('foursu_exon' in i) and ('scaled' not in i)]
-    foursu_intron_cols = [i for i in colnames if ('foursu_intron' in i) and ('scaled' not in i)]
-    total_exon_cols = [i for i in colnames if ('total_exon' in i) and ('scaled' not in i)]
-    total_intron_cols = [i for i in colnames if ('total_intron' in i) and ('scaled' not in i)]
-    synth_est_cols = [i for i in colnames if 'synth_est' in i]
-    decay_est_cols = [i for i in colnames if 'decay_est' in i]
-    decay_from_total_est_cols = [i for i in colnames if 'decay_simple_est' in i]
-    proc_est_cols = [i for i in colnames if 'proc_est' in i]
-
-    for cols in [synth_cols, deg_cols, proc_cols, foursu_exon_cols, foursu_intron_cols, total_exon_cols, total_intron_cols, synth_est_cols, decay_est_cols, decay_from_total_est_cols, proc_est_cols]:
-        for j in cols:
-            df['{0}_log10_{1}'.format('_'.join(j.split('_')[:-1]), j.split('_')[-1])] = df[j].apply(np.log10)
-
-    newcolnames = df.columns.values
-    synth_cols = [i for i in newcolnames if 'synthesis_rate_log10' in i]
-    deg_cols = [i for i in newcolnames if 'decay_rate_log10' in i]
-    proc_cols = [i for i in newcolnames if 'processing_rate_log10' in i]
-    foursu_exon_cols = [i for i in newcolnames if 'foursu_exon_counts_log10' in i]
-    foursu_intron_cols = [i for i in newcolnames if 'foursu_intron_counts_log10' in i]
-    total_exon_cols = [i for i in newcolnames if 'total_exon_counts_log10' in i]
-    total_intron_cols = [i for i in newcolnames if 'total_intron_counts_log10' in i]
-    synth_est_cols = [i for i in newcolnames if 'synth_est_log10' in i]
-    decay_est_cols = [i for i in newcolnames if 'decay_est_log10' in i]
-    decay_simple_est_cols = [i for i in newcolnames if 'decay_simple_est_log10' in i]
-    proc_est_cols = [i for i in newcolnames if 'proc_est_log10' in i]
-
-    df = df[synth_cols + deg_cols + proc_cols + foursu_exon_cols + foursu_intron_cols + total_exon_cols + total_intron_cols + synth_est_cols + decay_est_cols + decay_simple_est_cols + proc_est_cols]
-    
-    to_plot_dict = {'synthesis': synth_cols, 'decay': deg_cols, 'processing': proc_cols, 'foursu_exon': foursu_exon_cols, 'foursu_intron': foursu_intron_cols, 'total_exon': total_exon_cols, 'total_intron': total_intron_cols, 'synthesis_est': synth_est_cols, 'decay_est': decay_est_cols, 'decay_simple_est': decay_simple_est_cols, 'processing_est': proc_est_cols}
-  
-    #ii) make mini_dfs for each filtering combination and plot
-    num_exps = len(synth_cols)
-    pairs = [pair for pair in itertools.combinations(range(num_exps), 2)]
-    rep_comb = ['pearson_r2_rep%s_v_rep%s' % (i[1]+1, i[0]+1) for i in pairs]
-    header = ['rate', 'filtering_method', 'min_reads', 'num_genes'] + rep_comb
-    filter_summary_df = pd.DataFrame(columns = header)
+    #plot reproducibility at different filtering levels for each of the following
+    to_plot = ['synthesis', 'degradation', 'processing', 'TPM_exon_foursu', 'TPM_intron_foursu', 'TPM_exon_total', 'TPM_intron_total']
+    reps = [i for i in df.columns.levels[0] if i.startswith('rep')]
     gene_files = [os.path.join(gene_list_dir, i) for i in os.listdir(gene_list_dir) if i.endswith('.csv')]
 
     this_outdir = os.path.join(outdir, 'scatterplots')
-    if not os.path.exists(this_outdir):
-        os.makedirs(this_outdir)
-        for rate in to_plot_dict:
-            os.makedirs(os.path.join(this_outdir, rate))
+    os.makedirs(this_outdir, exist_ok = True)
+    for rate in to_plot:
+        os.makedirs(os.path.join(this_outdir, rate), exist_ok = True)
 
+    corr_results_dict = {}
+    n = 0
     for i in gene_files:
         read_type, co = os.path.basename(i).split('.csv')[0].split('_')[-2:]
-        #get empty dataframe with genes that passed the cutoff
-        passed_genes = pd.read_csv(i, names = ['txt'], index_col = 'txt')
-        #do inner join instead of left, because we'll already have gotten rid of the ERCC stds from df but these show up in passed
-        filtered_df = pd.merge(passed_genes, df, how = 'inner', left_index = True, right_index = True)
-        
-        #despite filtering for read count cutoff, still won't be able to get synthesis or decay rate for a lot of genes
-        #iii) Plot rate reprocibility
-        for rate in to_plot_dict:
-            fig = plt.figure(figsize = (8,8))
-            n = 1
-            corr_vals = []
-            for pair in pairs:
-                x_name = to_plot_dict[rate][pair[0]]
-                y_name = to_plot_dict[rate][pair[1]]
-               
-                #because np.log(0) = -inf instead of np.nan, need to convert these values
-                valid_df = filtered_df.replace([np.inf, -np.inf], np.nan, inplace = False)
-                valid_df.dropna(axis = 0, how = 'any', subset = [x_name, y_name], inplace = True)
+        #get dataframe with only the subset of genes that passed filter
+        passed_genes = set(pd.read_csv(i, names = ['txt'], index_col = 'txt').index.tolist())
+        filtered_df = df.loc[df.index.isin(passed_genes)]
+        for rate in to_plot:
+            #get location of each replicate from the hierachical index
+            these_cols = list(itertools.product(*[reps, [rate]]))
+            exp_name = '%s_%s_%s' % (rate, read_type, co)
+            filename = os.path.join(this_outdir, rate, '%s' % exp_name)
 
-                num_plotted = len(valid_df)
-                corr = valid_df[x_name].corr(valid_df[y_name])
-                r2_val = corr**2
-                corr_vals.append(r2_val)
-                ax = fig.add_subplot(num_exps - 1, num_exps - 1, n)
-                ax.scatter(valid_df[x_name], valid_df[y_name], color = 'k', s = 10)
-                ax.text(0.1, 0.9, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes)
-                ax.set_xlabel(x_name)
-                ax.set_ylabel(y_name)
-                n += 1
+            title = exp_name
+            #give index level 0 so that it will write 'rep_1', etc. in the axis titles
+            label_index_level = 0
+            axis_title_suffix = '%s (log10)' % rate
+            results = pipeline_aux.plot(filtered_df, cols = these_cols, plottype = 'multiscatter', logbase = 10, title = exp_name, label_index_level = 0, axis_title_suffix = axis_title_suffix, filename = filename)
+            exp_d = {'rate': rate, 'filtering_method': read_type, 'min_reads': co, 'num_genes': results['num_plotted']}
+            exp_d.update(results['corr_dict'])
+            corr_results_dict[n] = exp_d
+            n += 1
 
-            plt.savefig(os.path.join(this_outdir, rate, '%s_%s_%s.png' % (rate, read_type, co)))
-            plt.close(fig)
-            result_dict = {}
-            result_dict['rate'] = rate
-            result_dict['filtering_method'] = read_type
-            result_dict['min_reads'] = co
-            result_dict['num_genes'] = num_plotted
-            for j in range(0, len(rep_comb)):
-                result_dict[rep_comb[j]] = corr_vals[j]
-            filter_summary_df = filter_summary_df.append(result_dict, ignore_index = True)
-
+    #write summary file with number of genes passes and repllicate correlations
+    filter_summary_df = pd.DataFrame.from_dict(corr_results_dict, orient = 'index')
     filter_summary_df = filter_summary_df.round(4)
     filter_summary_df.to_csv(os.path.join(outdir, 'filtering_summary.csv'), index = False)
 
 def main():
     datafile = snakemake.input['all_data_file']
-    big_df = pd.read_csv(datafile, index_col = 'transcript')
+
+    df = pd.read_csv(datafile, header = [0,1])
+    df.set_index(('gene_info', 'transcript'), inplace = True, drop = False)
+    df.index.rename('transcript', inplace = True)
+
     #get set of ercc and sirv transcripts by name
-    all_genes = big_df.index
+    all_genes = df.index
     erccs = set([i for i in all_genes if i.startswith('ERCC')])
     sirvs = set([i for i in all_genes if i.startswith('SIRV')])
-    big_df = plot_spikeins(big_df, genes_dict = {'ERCC':erccs, 'SIRV':sirvs}, outdir = snakemake.params['plot_dir'])
-    plot_reproducibility(big_df, outdir = snakemake.params['plot_dir'], gene_list_dir = snakemake.params['genelist_dir'], min_reads = snakemake.params['min_reads'], min_read_type = snakemake.params['min_read_type'])
+    #running plot_spikeins() first removes the spikein genes from df, so they don't end up in the reproducibility scatter plots
+    plot_spikeins(df, genes_dict = {'ERCC':erccs, 'SIRV':sirvs}, outdir = snakemake.params['plot_dir'])
+    plot_reproducibility(df, outdir = snakemake.params['plot_dir'], gene_list_dir = snakemake.params['genelist_dir'], min_reads = snakemake.params['min_reads'], min_read_type = snakemake.params['min_read_type'])
 
 if __name__ == '__main__':
     main()

common_scripts/pipe_utils/pipeline_aux.py
@@ -39,7 +39,10 @@ def quick_barplot(data = None, y_col = None, x_col = None, divide_by = 1, percen
     xlabels = df[x_col]
     #lining the right side of text box up with tick looks best of options, still not great
     ax.set_xticklabels(xlabels, rotation = 45, ha = 'right')
+    plt.tight_layout()
     plt.savefig('%s.png' % outname)
+    plt.close(fig)
+
     return df
 
 def plot_genomic_region(coverage, chrom, start, end, strand, positions = None):
@@ -62,11 +65,13 @@ def plot_genomic_region(coverage, chrom, start, end, strand, positions = None):
 
     return ax
 
-def scatter_plot(df, x_col = None, y_col = None, x_name = None, y_name = None, filename = None, logbase = None):
+def scatter_plot(df, x_col = None, y_col = None, x_name = None, y_name = None, filename = None, logbase = None, title = None):
     '''
     Plot data contained in x_col and y_col with optional log transformation. Data will be prefiltered to remove nans
     '''
-    
+    #avoid modifying original dataframe if using inplace operations
+    df = df.copy()
+
     if logbase != None:
         if logbase == 10:
             df['x_log'] = df[x_col].apply(np.log10)
@@ -78,7 +83,7 @@ def scatter_plot(df, x_col = None, y_col = None, x_name = None, y_name = None, f
             raise NotImplementedError('logbase %s not supported' % logbase)
         x_col = 'x_log'
         y_col = 'y_log'
-        
+
     #because np.log(0) = -inf instead of np.nan, need to convert these values
     df.replace([np.inf, -np.inf], np.nan, inplace = True)
     df.dropna(axis = 0, how = 'any', subset = [x_col, y_col], inplace = True)
@@ -88,11 +93,193 @@ def scatter_plot(df, x_col = None, y_col = None, x_name = None, y_name = None, f
     fig = plt.figure(figsize = (8, 8))
     ax = fig.add_subplot(111)
     ax.scatter(df[x_col], df[y_col], color = 'k', s = 10)
-    ax.text(0.1, 0.9, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes)
+    #can we specify the fontsize in the .mplstyle file?
+    #r'$\alpha_i > \beta_i$'
+    #ax.text(0.1, 0.9, r'$r^2 = {0} n = {1}$'.format(r2_val, num_plotted), transform = ax.transAxes, fontsize = 12)
+    ax.text(0.1, 0.9, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes, fontsize = 12)
+
+    #ax.text(0.1, 0.9, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes, fontsize = 12)
     ax.set_xlabel(x_name)
     ax.set_ylabel(y_name)
+    plt.title(title)
     if filename != None:
         plt.savefig('%s.png' % filename)
         plt.close(fig)
     else:
         return ax
+
+def limit_df(df, colnames = None, min_val = None, max_val = None):
+    '''
+    Filter out data that is outside given min and max values
+    '''
+
+    for col in colnames:
+        df = df[df[col].between(min_val, max_val, inclusive = True)].copy()
+
+    df.dropna(subset = colnames, inplace = True)
+
+    return df
+
+def clean_df(df, cols = None):
+    '''
+    Replace inf values with nan & drop nan-containing rows over given columns
+    This needs to be done before analysis and/or after log-transform
+    '''
+    df.replace([np.inf, -np.inf], np.nan, inplace = True)
+    df.dropna(axis = 0, how = 'any', subset = cols, inplace = True)
+
+def cdf_plot(df, x_col = None, bg_group = 'all', group_col = None, nbins = 100, x_label = None, y_label = None, filename = None, logbase = None, title = None):
+    '''
+    Plot data in given column and a group_by variable as CDF
+    x_col = the name of the column containing the data
+    group_col = the name of the column containing the classification variable, e.g. short, long
+    nbins = number of bins to use
+    '''
+    fig = plt.figure(figsize = (8, 8))
+    ax = fig.add_subplot(111)
+
+    groups = df[group_col].unique()
+    groups = sorted(groups)
+
+    #bring bg group to front if we are not using a specific bg group
+    if bg_group != 'all':
+        groups.insert(0, groups.pop(groups.index(bg_group)))
+        first_df = None
+        #build first df from first group
+    #otherwise the first group will be the whole dataset, named 'all'
+    else:
+        groups.pop(groups.index('bg'))
+        groups.insert(0, 'all')
+        first_df = df
+
+    all_bins = []
+    all_cdfs = []
+    handles = []
+    labels = []
+
+    for i in range(0, len(groups)):
+        if i == 0:
+            if first_df is not None:
+                sub_df = first_df
+            else:
+                sub_df = df[df[group_col] == groups[i]].copy()
+        else:
+            sub_df = df[df[group_col] == groups[i]].copy()
+
+        data = sub_df[x_col]
+        counts, bin_edges = np.histogram(data, bins = nbins, normed = True)
+        cdf = np.cumsum(counts)
+        all_cdfs.append(cdf)
+        all_bins.append(bin_edges)
+        l, = plt.plot(bin_edges[1:], cdf/cdf[-1])
+        handles.append(l)
+
+        if i == 0:
+            if bg_group != 'all':
+                bg_data = data
+            else:
+                bg_data = df[x_col]
+            labels.append('{:}, n={:}'.format(groups[i], len(data)))
+        else:
+            stat, pval = sp.stats.ks_2samp(bg_data, data)
+            labels.append('{:}, n={:}, p={:.2E}'.format(groups[i], len(data), Decimal(pval)))
+
+    plt.legend(handles, labels)
+
+    #ax.text(0.1, 0.9, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes, fontsize = 12)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.title(title)
+    if filename != None:
+        plt.savefig('%s.png' % filename)
+        plt.close(fig)
+    else:
+        return ax
+
+def seaborn_box():
+	'''Seaborn style box plot'''
+	#boxplot(df, xname, yname)
+	sns.set(style='ticks')
+	fig=plt.figure(figsize=(8,4))
+	ax=sns.boxplot(data=data, showfliers=False)
+
+	#sns.boxplot(x=xname, y=yname, data=df, showfliers=False)
+
+	#sns.boxplot(x=xname, y=yname, hue=colname, data=df, showfliers=False)
+	#sns.despine(offset=10, trim=True)
+	sns.despine(offset=10)
+	return ax
+
+def multiscatter_plot(df, cols = None, label_index_level = None, axis_title_suffix = None, filename = None, title = None):
+    '''
+    Make a multiscatter plot of all the combinations in given columns
+    Also retun correlations for each
+    '''
+    #store results of each replicate as correlation dict
+    corr_dict = {}
+    num_plotted = len(df)
+    pairs =  [pair for pair in itertools.combinations(range(len(cols)), 2)]
+    fig = plt.figure(figsize = (8,8))
+    n = 1
+    for pair in pairs:
+        xi = pair[0]
+        yi = pair[1]
+        xname = cols[pair[0]]
+        yname = cols[pair[1]]
+        corr = df[xname].corr(df[yname])
+        r2_val = corr**2
+        corr_dict['%s_v_%s' % (yname[label_index_level], xname[label_index_level])] = r2_val
+        ax = fig.add_subplot(len(pairs) - 1, len(pairs) - 1, n)
+        ax.scatter(df[xname], df[yname], color = 'k', s = 10)
+        ax.text(0.1, 0.8, 'r2 = %1.3f\nn = %s' % (r2_val, num_plotted), transform = ax.transAxes)
+        ax.set_xlabel('%s %s' % (xname[label_index_level], axis_title_suffix))
+        ax.set_ylabel('%s %s' % (yname[label_index_level], axis_title_suffix))
+        n += 1
+
+    plt.tight_layout()
+    plt.suptitle(title)
+    plt.subplots_adjust(top = 0.92)
+    plt.savefig('%s.png' % filename)
+    plt.close(fig)
+
+    return {'ax': ax, 'corr_dict': corr_dict, 'num_plotted': num_plotted}
+
+def log_transform(df, cols = None, logbase = None, label_index_level = None):
+    '''Transform columns with given logbase and return new df and column names'''
+    if logbase in np_log_transforms:
+        logcols = []
+        for col in cols:
+            #if col is a tuple, we're dealing with a hierarchical index
+            #for now I'm assuming the next level (label_index_level + 1) will have the name of the data to transform
+            if type(col) == tuple:
+                newcol = (col[label_index_level], '%s_log' % col[label_index_level+1])
+            else:
+                newcol = '%s_log' % col
+
+            df[newcol] = df[col].apply(np_log_transforms[logbase])
+            logcols.append(newcol)
+    else:
+        raise NotImplementedError('logbase %s not supported' % logbase)
+
+    #shouldn't need to retun df, as it should be modified here
+    return logcols
+
+def plot(df, cols = None, plottype = None, logbase = None, title = None, label_index_level = 0, axis_title_suffix = None, filename = None):
+    '''
+    Given a df and plottype, clean up data and then send to plotting function
+    df = pandas dataframe, cols = list of columns with data, plottype = 'scatter', etc.
+    '''
+    #make a copy of the df here. All subsequent operations will modify this copy
+    df = df[cols].copy()
+    clean_df(df, cols = cols)
+
+    #get new log-transformed columns if required
+    if logbase != None:
+        cols = log_transform(df, cols = cols, logbase = logbase, label_index_level = label_index_level)
+        clean_df(df, cols = cols)
+
+    results = plot_fxn_dict[plottype](df, cols = cols, title = title, label_index_level = label_index_level, axis_title_suffix = axis_title_suffix, filename = filename)
+    return results
+
+plot_fxn_dict = {'multiscatter':multiscatter_plot}
+np_log_transforms = {2:  np.log2, 10: np.log10}